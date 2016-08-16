001ee331ac6e31f8fe235190171ee629f13b79e0
wzlUp <1076500039@qq.com>
2016-08-16 19:42:24 +0800

    修改android标签显示在底部

--- www/js/app.js
+++ www/js/app.js
@@ -28,7 +28,24 @@ angular.module('starter', ['ionic'])
  * 2.配置子级页面的规则是：父路由.子路由
  * 3.并且增加了一个views字典，字段的key值对应着我们前面所说的tabs.html中ion-nav-view的name属性，表示该路由跳转的页面属于某个tab的子页面
  */
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+
+    /**
+     * 设置ios标签在顶部，android时标签在底部
+     */
+    $ionicConfigProvider.platform.ios.tabs.style('standard');
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('standard');
+
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+    $ionicConfigProvider.platform.ios.views.transition('ios');
+    $ionicConfigProvider.platform.android.views.transition('android');
 
   $stateProvider
     .state('home', {
@@ -46,7 +63,7 @@ angular.module('starter', ['ionic'])
       }
     })
     .state('home.detail',{
-      url: '/detail',
+      url: '/find/detail',
       views:{
         'home-find':{
           templateUrl: 'templates/home-detail.html'
